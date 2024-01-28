88480f89628502637851c0b321ddd98cf70401e8
Saurabh Anand Srivastava <saurabh.anand@synchsoft.in>
2024-01-28 23:44:01 +0530

    Bug Fixes

--- Sources/OnlineAudioPlayer/AudioPlayer/SynchAudioPlayer.swift
+++ Sources/OnlineAudioPlayer/AudioPlayer/SynchAudioPlayer.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SynchAudioPlayer: NSObject {
+public class SynchAudioPlayer: NSObject {
     static let shared = SynchAudioPlayer()
     var audioPlayer: AudioPlayer!
     private override init(){}
